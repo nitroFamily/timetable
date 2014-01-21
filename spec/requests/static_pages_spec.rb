c96fcf77afa13be2ca77f6d97a9469f79d4d41f8
Aleksandr <samaydanob@gmail.com>
2014-01-21 11:44:03 +0400

    Добавлены стили, Bootstrap 2.3.2

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,27 +2,33 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
+
   describe "Home page" do
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Timetable')
-      expect(page).to have_title("Timetable | Home")
-    end
+    before { visit root_path }
+
+    it { should have_content('Timetable') }
+    it { should have_title(full_title('Главная')) }
   end
 
   describe "Help page" do
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content("Help")
-      expect(page).to have_title("Timetable | Help")
-    end
+    before { visit help_path }
+
+    it { should have_content('Help') }
+    it { should have_title(full_title('Помощь')) }
   end
 
   describe "About page" do
-    it "should have the content 'About'" do
-      visit '/static_pages/about'
-      expect(page).to have_content("About")
-      expect(page).to have_title("Timetable | About")
-    end
+    before { visit about_path }
+
+    it { should have_content('About') }
+    it { should have_title(full_title('О нас')) }
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Контакты')) }
   end
 end
